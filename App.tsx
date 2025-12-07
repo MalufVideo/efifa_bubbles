9211c3cde51d914d9ce36d07620bb5433d741d58
MalufVideo <nelsonoliveira@msi.(none)>
2025-12-06 22:43:10 -0300

    Remove LandingPage - redirect root to admin

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AdminPage } from "./pages/AdminPage";
 import { BroadcastPage } from "./pages/BroadcastPage";
-import { LandingPage } from "./pages/LandingPage";
 
 const App: React.FC = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Navigate to="/admin" replace />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/broadcast" element={<BroadcastPage />} />
       </Routes>
